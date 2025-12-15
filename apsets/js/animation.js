26782fe69954858e60d8e180e44082d9c25c1453
iro-has <shunto2911@gmail.com>
2025-12-15 19:45:31 +0900

    概要をヘッダー上部に移動 おみくじ機能のレイアウト変更。

--- apsets/js/animation.js
+++ apsets/js/animation.js
@@ -1,12 +1,12 @@
 const images = [
     './images/background-images/S__58540035_0.jpg',
-    './images/background-images/S__58540041_0.jpg',
+    './images/background-images/S__58540037_0.jpg',
     './images/background-images/S__58540042_0.jpg',
     './images/background-images/S__58540050_0.jpg'
 ];
 
 let index = 0;
-const bg = document.getElementById("head-slide");
+const bg = document.getElementById("header-slide");
 
 function changeBackground() {
     bg.style.backgroundImage = `url(${images[index]})`;
